1aa80de3db3175b1749b15e553f8416b6f51e782
Clev <105460616+clevartist@users.noreply.github.com>
2024-08-29 12:05:17 +0300

    Create HomeGUI.js

---
+++ components/HomeGUI.js
@@ -0,0 +1,42 @@
+import { View, StyleSheet } from "react-native";
+
+export default function HomeGUI({ resetCamera }) {
+  return (
+    <>
+      <View style={styles.guiReset}>
+        <Pressable
+          onPress={resetCamera}
+          style={{ backgroundColor: "white", padding: 10, borderRadius: 5 }}
+        >
+          <Text style={{ width: 90 }}>Reset camera</Text>
+        </Pressable>
+      </View>
+      <View style={styles.guiTitle}>
+        <Text style={styles.anatolens}>AnatoLens</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  guiReset: {
+    position: "absolute",
+    alignItems: "center",
+    left: 0,
+    right: 0,
+    bottom: 70,
+  },
+  guiTitle: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  anatolens: {
+    color: "#052659",
+    fontSize: 30,
+    width: 150,
+  },
+});
