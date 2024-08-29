c81e8261b53e43d90f8be8644900359056073b51
Clev <105460616+clevartist@users.noreply.github.com>
2024-08-29 10:23:38 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,88 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { Text, View, SafeAreaView, StyleSheet, Pressable } from "react-native";
+import { useFrame, useThree, Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import Donut from "./components/Donut";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
-  function Torus(props) {
-    const [scaleValue, setScaleValue] = useState(false);
-    const [targetScale, setTargetScale] = useState(0.7);
-    const [currentScale, setCurrentScale] = useState(0.7);
-    const [wire, setWire] = useState(false);
+  const cameraDefaultPosition = new THREE.Vector3(0, 0, 5);
+  const [цель, установитьЦель] = useState(cameraDefaultPosition.clone());
+  const [lookAtObject, setLookAtObject] = useState(false);
+  const offsetRef = useRef(new THREE.Vector3());
 
-    const mesh = useRef();
-    const scaleSpeed = 0.03;
+  function CameraZoom({ цель }) {
+    const { camera } = useThree();
 
-    useFrame((state, delta) => {
-      // Gradually adjust the scale towards the target scale
-      setCurrentScale((prevScale) => {
-        const newScale = prevScale + (targetScale - prevScale) * scaleSpeed;
-        return newScale;
-      });
-
-      mesh.current.rotation.y += delta * 2; // Rotate the torus
-      mesh.current.scale.set(currentScale, currentScale, currentScale); // Update the scale on x, y, z
+    useFrame(() => {
+      if (цель) {
+        const offset = цель.clone().add(new THREE.Vector3(2, 2, 2));
+        offsetRef.current.copy(offset);
+        camera.position.lerp(offset, 0.05);
+        camera.lookAt(lookAtObject ? цель : 0, 0, 0);
+        camera.updateProjectionMatrix();
+      }
     });
 
-    const changeScale = () => {
-      setScaleValue(!scaleValue);
-      setTargetScale(scaleValue ? 0.7 : 1.4); // Toggle between 0.7 and 1 on click
-      setWire(!wire);
-    };
-
-    return (
-      <mesh {...props} ref={mesh} onClick={changeScale}>
-        <torusGeometry />
-        <meshStandardMaterial wireframe={wire} color={"#cd965f"} />
-      </mesh>
-    );
+    return null;
   }
 
+  const resetCamera = () => {
+    установитьЦель(cameraDefaultPosition.clone());
+    setLookAtObject(false);
+  };
+
+  useEffect(() => {
+    console.log("camera position: ", offsetRef.current);
+    console.log("camera target: ", цель);
+    console.log("lookAtObject: ", lookAtObject);
+  }, [цель, lookAtObject, offsetRef]);
+
   return (
-    <Canvas style={{ backgroundColor: "black" }}>
-      <ambientLight />
-      <pointLight color="#fff" position={[10, 10, 10]} intensity={1000} />
-      <Torus position={[0, 0, 0]} />
-    </Canvas>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Canvas style={{ flex: 1, backgroundColor: "#021024" }}>
+        <ambientLight />
+        <pointLight color="#fff" position={[10, 10, 10]} intensity={1000} />
+        <Donut
+          установитьЦель={установитьЦель}
+          setLookAtObject={setLookAtObject}
+          lookAtObject={lookAtObject}
+        />
+        <CameraZoom цель={цель} />
+      </Canvas>
+      <View style={styles.guiReset}>
+        <Pressable
+          onPress={resetCamera}
+          style={{ backgroundColor: "white", padding: 10, borderRadius: 5 }}
+        >
+          <Text style={{ width: 90 }}>Reset camera</Text>
+        </Pressable>
+      </View>
+      <View style={styles.guiTitle}>
+        <Text style={styles.anatolens}>AnatoLens</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  guiReset: {
+    position: "absolute",
+    alignItems: "center",
+    left: 0,
+    right: 0,
+    bottom: 70,
+  },
+  guiTitle: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  anatolens: {
+    color: "#052659",
+    fontSize: 30,
+    width: 150,
+  },
+});
