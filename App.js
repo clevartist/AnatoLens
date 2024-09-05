d0fcbee5819800f09fa325b5e32e10750f7b766c
Clev <105460616+clevartist@users.noreply.github.com>
2024-09-06 01:20:21 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import { View, SafeAreaView, StatusBar } from "react-native";
+import { useFrame, useThree, Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import Donut from "./components/Donut";
+import { useEffect, useRef, useState } from "react";
+import HomeGUI from "./components/HomeGUI";
+import Info from "./components/Info";
+import GlobalStyles from "./GlobalStyles";
+import bodyParts from "./components/InfoContent";
+
+export default function App() {
+  const cameraDefaultPosition = new THREE.Vector3(0, 0, 5);
+  const [цель, установитьЦель] = useState(cameraDefaultPosition.clone());
+  const [lookAtObject, setLookAtObject] = useState(false);
+  const [offsetValue, setOffsetValue] = useState(new THREE.Vector3());
+  const offsetRef = useRef(new THREE.Vector3());
+  const [dedicatedID, setDedicatedID] = useState(0);
+  const [info, setInfo] = useState(bodyParts);
+
+  function CameraZoom({ цель }) {
+    const { camera } = useThree();
+
+    useFrame(() => {
+      if (цель) {
+        offsetRef.current.copy(offsetValue);
+        camera.position.lerp(lookAtObject ? offsetValue : цель, 0.05);
+        camera.lookAt(lookAtObject ? цель : 0, 0, 0);
+        camera.updateProjectionMatrix();
+      }
+    });
+
+    return null;
+  }
+
+  const resetCamera = () => {
+    установитьЦель(cameraDefaultPosition.clone());
+    setLookAtObject(false);
+  };
+
+  useEffect(() => {
+    console.log("camera position:", offsetRef.current);
+    console.log("camera target:", цель);
+    console.log("lookAtObject:", lookAtObject);
+  }, [цель, lookAtObject, offsetRef]);
+
+  useEffect(() => {
+    const filtered = bodyParts.find((part) => part.id === dedicatedID);
+    setInfo(filtered);
+  }, [dedicatedID]);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#021024" }}>
+      <StatusBar color="white" backgroundColor={"#021024"} />
+      <SafeAreaView style={GlobalStyles.androidSafeZone}>
+        <Canvas style={{ flex: 1, backgroundColor: "#021024" }}>
+          <ambientLight />
+          <pointLight color="#fff" position={[10, 10, 10]} intensity={1000} />
+          <Donut
+            установитьЦель={установитьЦель}
+            setLookAtObject={setLookAtObject}
+            lookAtObject={lookAtObject}
+            setOffsetValue={setOffsetValue}
+            setDedicatedID={setDedicatedID}
+          />
+          <CameraZoom цель={цель} />
+        </Canvas>
+        {lookAtObject && <Info info={info} />}
+        <View>
+          <HomeGUI resetCamera={resetCamera} lookAtObject={lookAtObject} />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
