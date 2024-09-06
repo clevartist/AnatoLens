0c9f056551de772024b9ba58a41e611637f188a1
clevartist <isomdez@gmail.com>
2024-09-06 20:55:15 +0300

    remove useState

--- components/Donut.js
+++ components/Donut.js
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import * as THREE from "three";
 
 export default function Donut({
