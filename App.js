bda2fcc456ceb510a641e725b7d70f3c9e697e5a
clevartist <isomdez@gmail.com>
2024-09-06 20:20:02 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import { View, SafeAreaView, StatusBar } from "react-native";
+import { useFrame, useThree, Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import Donut from "./components/Donut";
+import { useEffect, useRef, useState } from "react";
+import HomeGUI from "./components/HomeGUI";
+import Info from "./components/Info";
+import GlobalStyles from "./GlobalStyles";
+import bodyParts from "./components/InfoContent";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const cameraDefaultPosition = new THREE.Vector3(0, 0, 5);
+  const [цель, установитьЦель] = useState(cameraDefaultPosition.clone());
+  const [lookAtObject, setLookAtObject] = useState(false);
+  const [offsetValue, setOffsetValue] = useState(new THREE.Vector3());
+  const offsetRef = useRef(new THREE.Vector3());
+  const [dedicatedID, setDedicatedID] = useState(0);
+  const [info, setInfo] = useState(bodyParts);
+
+  const [loaded, error] = useFonts({
+    DESIGNER: require("./assets/fonts/DESIGNER.otf"),
+  });
+
+  function CameraZoom({ цель }) {
+    const { camera } = useThree();
+
+    useFrame(() => {
+      if (цель) {
+        offsetRef.current.copy(offsetValue);
+        camera.position.lerp(lookAtObject ? offsetValue : цель, 0.05);
+        camera.lookAt(lookAtObject ? цель : 0, 0, 0);
+        camera.updateProjectionMatrix();
+      }
+    });
+
+    return null;
+  }
+
+  function resetCamera() {
+    установитьЦель(cameraDefaultPosition.clone());
+    setLookAtObject(false);
+  }
+
+  useEffect(() => {
+    console.log("camera position:", offsetRef.current);
+    console.log("camera target:", цель);
+    console.log("lookAtObject:", lookAtObject);
+
+    if (lookAtObject) {
+      const filtered = bodyParts.find((part) => part.id === dedicatedID);
+      setInfo(filtered);
+    }
+    if (!lookAtObject) {
+      setInfo(null);
+    }
+
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [цель, lookAtObject, offsetRef, dedicatedID, loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#021024" }}>
+      <StatusBar color="white" backgroundColor={"#021024"} />
+      <SafeAreaView style={GlobalStyles.androidSafeZone}>
+        <Canvas style={{ flex: 1, backgroundColor: "#021024" }}>
+          <ambientLight />
+          <pointLight color="#fff" position={[10, 10, 10]} intensity={1000} />
+          <Donut
+            установитьЦель={установитьЦель}
+            setLookAtObject={setLookAtObject}
+            lookAtObject={lookAtObject}
+            setOffsetValue={setOffsetValue}
+            setDedicatedID={setDedicatedID}
+          />
+          <CameraZoom цель={цель} />
+        </Canvas>
+        {lookAtObject && <Info info={info} />}
+        <View>
+          <HomeGUI resetCamera={resetCamera} lookAtObject={lookAtObject} />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
---
+++ components/Donut.js
@@ -0,0 +1,62 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import * as THREE from "three";
+
+export default function Donut({
+  установитьЦель,
+  setLookAtObject,
+  lookAtObject,
+  setOffsetValue,
+  setDedicatedID,
+}) {
+  function Torus(props) {
+    const mesh = useRef();
+    useFrame((state, delta) => {
+      if (!lookAtObject) {
+        mesh.current.rotation.y += delta / 1.5;
+      }
+    });
+
+    return (
+      <mesh {...props} ref={mesh} scale={0.7}>
+        <torusGeometry />
+        <meshStandardMaterial color={"#cd965f"} />
+        <Dot
+          position={[1, 1, 1]}
+          offsetValue={new THREE.Vector3(3, 3, 2)}
+          scalarValue={-2}
+          dedicatedID={2}
+        />
+        <Dot
+          position={[-1, -1, -1]}
+          offsetValue={new THREE.Vector3(-1, -2, -2)}
+          scalarValue={0}
+          dedicatedID={1}
+        />
+      </mesh>
+    );
+  }
+
+  function Dot({ position, offsetValue, scalarValue, dedicatedID }) {
+    const handleClick = (event) => {
+      const currentPosition = event.object.position.clone();
+      setLookAtObject(true);
+      установитьЦель(currentPosition.addScalar(scalarValue));
+      setOffsetValue(offsetValue);
+      setDedicatedID(dedicatedID);
+    };
+
+    return (
+      <mesh position={position} scale={0.2} onClick={handleClick}>
+        <sphereGeometry />
+        <meshStandardMaterial color={"white"} />
+      </mesh>
+    );
+  }
+
+  return (
+    <>
+      <Torus position={[0, 0, 0]} />
+    </>
+  );
+}
