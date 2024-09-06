935ea7c7771c91c8bb83a79f736a346a2d2fa33c
Clev <105460616+clevartist@users.noreply.github.com>
2024-09-06 20:23:42 +0300

    Delete GlobalStyles.js

--- GlobalStyles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Platform } from "react-native";
-
-export default styles = StyleSheet.create({
-  androidSafeZone: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? 36 : 0,
-  },
-});
