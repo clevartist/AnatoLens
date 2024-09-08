0d071e411d8e32074611138f59a382dd3c7940f0
clevartist <isomdez@gmail.com>
2024-09-08 17:51:54 +0300

    h

--- components/screens/BodyPart.js
+++ components/screens/BodyPart.js
@@ -1,7 +1,6 @@
-import { View, SafeAreaView } from "react-native";
+import { View, SafeAreaView, Pressable, Text } from "react-native";
 import { Canvas } from "@react-three/fiber";
-import { useMemo } from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import GlobalStyles from "../../GlobalStyles";
 
 const geometryMenu = {
@@ -10,18 +9,26 @@ const geometryMenu = {
 };
 
 export default function BodyPart() {
+  const navigation = useNavigation();
   const route = useRoute();
 
-  const infoGeometry = route.params?.info.geometry;
-
-  const DecidedGeometry = useMemo(
-    () => geometryMenu[infoGeometry],
-    [infoGeometry]
-  );
+  const DecidedGeometry = geometryMenu[route.params.info.geometry];
 
   return (
     <View style={{ flex: 1, backgroundColor: "#021024" }}>
       <SafeAreaView style={GlobalStyles.androidSafeZone}>
+        <View>
+          <Pressable
+            style={{
+              position: "absolute",
+              top: 0,
+              zIndex: 10,
+            }}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={{ color: "white" }}>go back</Text>
+          </Pressable>
+        </View>
         <Canvas style={{ flex: 1, backgroundColor: "#021024" }}>
           <ambientLight />
           <pointLight color="#fff" position={[10, 10, 10]} intensity={1000} />
